d529acbfedb52107727caee279f5fb3c89225660
Matthew Dierker <mvd7793@gmail.com>
2014-01-03 14:28:34 -0600

    first commit

---
+++ icantdothatdave.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  imSorryDaveImAfraidICantDoThat();
+
+  // Facebook doesn't reload almost ever, so we have to check repeatedly
+  setInterval(imSorryDaveImAfraidICantDoThat, 10000);
+});
+
+function imSorryDaveImAfraidICantDoThat() {
+  // Fetch comments from Dave
+  var daveComments = $('li.UFIComment div.UFICommentContent[dave!=true]').has('a.UFICommentActorName[href="https://www.facebook.com/davefontenot"]');
+  $.each(daveComments, function(idx, comment) {
+    // Mark that we've processed this already
+    $(comment).attr('dave', true);
+
+    var text = $(comment).find('span span span').html();
+    if (text) {
+      var match = text.match(/he(l+)\syeah.*/i);
+
+      // Test if Dave's comment matches what it should
+      if (match) {
+        var commentId = $(comment).attr('data-reactid').match(/\{comment(\d*_?\d*)\}/)[1];
+        var num_ls = match[1].length;
+
+        // Log and move on
+        $.get('http://hellyeah.muri.ca/report?comment_id=' + commentId + '&num_ls=' + num_ls);
+      }
+    }
+  });
+}
